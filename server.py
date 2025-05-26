45337e60fd096c78033ebffa116b5032738b788e
shuheikomatsuki <shuheijy@gmail.com>
2025-05-27 08:50:17 +0900

    デバッグのために変更していた定数の値を元に戻した

--- server.py
+++ server.py
@@ -55,10 +55,10 @@ token_list = {}
 list_lock = threading.Lock()
 
 # UDPクライアントのタイムアウト時間
-UDP_CLIENT_TIME_OUT = 15 # デバッグ用に一時的に短縮
+UDP_CLIENT_TIME_OUT = 120
 
 # 最終メッセージ送信時間を定期的に確認
-LAST_MESSAGE_TIME = 5 # デバッグ用に一時的に短縮
+LAST_MESSAGE_TIME = 20
 
 # TCPサーバー
 
