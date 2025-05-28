998b1c79100cad82b231a8ef5b37fe440af3d963
Franco-M-Enzian <emf0000b@gmail.com>
2025-05-28 14:12:33 +0900

    タイムアウト時間、定期的な確認時間を元に戻す

--- server.py
+++ server.py
@@ -46,10 +46,10 @@ token_list = {}
 list_lock = threading.Lock()
 
 # UDPクライアントのタイムアウト時間
-UDP_CLIENT_TIME_OUT = 10
+UDP_CLIENT_TIME_OUT = 120
 
 # 最終メッセージ送信時間を定期的に確認
-LAST_MESSAGE_TIME = 5
+LAST_MESSAGE_TIME = 20
 
 # システムメッセージ
 SYSTEM_MESSAGE = "SYSTEM_MESSAGE_TIME_OUT"
